851a4b038d4c94595375c498993841475dbbec46
Matti Schneider <hi@mattischneider.fr>
2017-06-11 17:31:50 +0200

    Detect userMedia on start click

--- index.js
+++ index.js
@@ -131,81 +131,76 @@ function startRecording() {
   // Pen "VU Meter from MIC Input" from Travis Holliday
   // https://codepen.io/travisholliday/pen/gyaJk
 
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-  if (navigator.getUserMedia) {
-    navigator.getUserMedia({ audio: true }, function (stream) {
-        audioContext = new AudioContext();
-        analyser = audioContext.createAnalyser();
-        microphone = audioContext.createMediaStreamSource(stream);
-        javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+  navigator.getUserMedia({ audio: true }, function (stream) {
+      audioContext = new AudioContext();
+      analyser = audioContext.createAnalyser();
+      microphone = audioContext.createMediaStreamSource(stream);
+      javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
 
-        analyser.smoothingTimeConstant = 0.8;
-        analyser.fftSize = 1024;
+      analyser.smoothingTimeConstant = 0.8;
+      analyser.fftSize = 1024;
 
-        microphone.connect(analyser);
-        analyser.connect(javascriptNode);
-        javascriptNode.connect(audioContext.destination);
+      microphone.connect(analyser);
+      analyser.connect(javascriptNode);
+      javascriptNode.connect(audioContext.destination);
 
-        javascriptNode.onaudioprocess = function() {
-          var array = new Uint8Array(analyser.frequencyBinCount);
-          analyser.getByteFrequencyData(array);
-          var values = 0;
+      javascriptNode.onaudioprocess = function() {
+        var array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        var values = 0;
 
-          var length = array.length;
-          for (var i = 0; i < length; i++) {
-            values += array[i];
+        var length = array.length;
+        for (var i = 0; i < length; i++) {
+          values += array[i];
+        }
+
+        var average = values / length;
+
+        /***** BRAIN OF THE LONEPOD *****/
+        function getRandomArbitrary(min, max) {
+          return Math.floor(Math.random() * (max - min) + min);
+        }
+        /* BUGS :
+        PLAY("sounds/indistinct1.mp4");
+        PLAY("sounds/grognement2.wav");
+        PLAY("sounds/exclamation.mp4"); //pas d'erreur mais pas de son !!!
+        */
+        if (average > 50) {
+          switch (getRandomArbitrary(0, 4)) {
+            case 1:
+              PLAY("sounds/chut1.mp4");
+              break;
+            case 2:
+              PLAY("sounds/hush.wav");
+              break;
+            case 3:
+              PLAY("sounds/gromellement.mp4");
+              break;
+            default:
+              PLAY("sounds/chut2.mp4");
+              break;
           }
-
-          var average = values / length;
-
-          /***** BRAIN OF THE LONEPOD *****/
-          function getRandomArbitrary(min, max) {
-            return Math.floor(Math.random() * (max - min) + min);
+        } else {
+          switch (getRandomArbitrary(0, 6)) {
+            case 1:
+              PLAY("sounds/toux2.mp4");
+              break;
+            case 3:
+              PLAY("sounds/grognement2.mp4");
+              break;
+            case 4:
+              PLAY("sounds/toux2.mp4");
+              break;
+            default:
+              PLAY("sounds/sifflement.mp4");
+              break;
           }
-          /* BUGS :
-          PLAY("sounds/indistinct1.mp4");
-          PLAY("sounds/grognement2.wav");
-          PLAY("sounds/exclamation.mp4"); //pas d'erreur mais pas de son !!!
-          */
-          if (average > 50) {
-            switch (getRandomArbitrary(0, 4)) {
-              case 1:
-                PLAY("sounds/chut1.mp4");
-                break;
-              case 2:
-                PLAY("sounds/hush.wav");
-                break;
-              case 3:
-                PLAY("sounds/gromellement.mp4");
-                break;
-              default:
-                PLAY("sounds/chut2.mp4");
-                break;
-            }
-          } else {
-            switch (getRandomArbitrary(0, 6)) {
-              case 1:
-                PLAY("sounds/toux2.mp4");
-                break;
-              case 3:
-                PLAY("sounds/grognement2.mp4");
-                break;
-              case 4:
-                PLAY("sounds/toux2.mp4");
-                break;
-              default:
-                PLAY("sounds/sifflement.mp4");
-                break;
-            }
-           }
-          /********************************/
-        };
-      },
-      function(err) {
-        console.log("The following error occured: " + err.name);
-      }
-    );
-  } else {
-    console.log("getUserMedia not supported");
-  }
+         }
+        /********************************/
+      };
+    },
+    function(err) {
+      console.log("The following error occured: " + err.name);
+    }
+  );
 }
